59d09121ba89745162b7741de28ef338a273cce4
Jared <jaredarodriguez@gmail.com>
2019-11-26 15:05:10 -0600

    gapi initiated

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class GoogelAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '865039411830-gjtip7phpaf4isdpi4u64slgmt10sjv2.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
     render() {
         return <div>Google Auth</div>
     }
